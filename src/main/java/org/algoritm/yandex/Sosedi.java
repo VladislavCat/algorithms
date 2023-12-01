4ac23dc93fea272d87d69a5139ba141a9b8eccec
Vladislav <k.vladislav@astondevs.ru>
2023-12-01 11:16:49 +0300

    Финальный штрих

--- src/main/java/org/algoritm/yandex/Sosedi.java
+++ src/main/java/org/algoritm/yandex/Sosedi.java
@@ -18,20 +18,20 @@ public class Sosedi {
         int columnIndex = (scanner.nextInt());
         int[] rsl = columnIndex == 0 || stringIndex == 0 ? new int[2] : new int[4];
         if (columnIndex != 0 && columnIndex + 1 < matrix[stringIndex].length) {
-            rsl[0] = matrix[stringIndex][columnIndex - 1];
-            rsl[1] = matrix[stringIndex][columnIndex + 1];
+            rsl[2] = matrix[stringIndex][columnIndex - 1];
+            rsl[3] = matrix[stringIndex][columnIndex + 1];
         } else if (columnIndex != 0) {
-            rsl[0] = matrix[stringIndex][columnIndex - 1];
+            rsl[1] = matrix[stringIndex][columnIndex - 1];
         } else if (columnIndex + 1 < matrix[stringIndex].length) {
-            rsl[0] = matrix[stringIndex][columnIndex + 1];
+            rsl[1] = matrix[stringIndex][columnIndex + 1];
         }
         if (stringIndex != 0 && stringIndex + 1 < matrix.length) {
-            rsl[2] = matrix[stringIndex - 1][columnIndex];
-            rsl[3] = matrix[stringIndex + 1][columnIndex];
-        } else if (stringIndex != 0) {
-            rsl[1] = matrix[stringIndex - 1][columnIndex];
-        } else if (stringIndex + 1 < matrix.length) {
+            rsl[0] = matrix[stringIndex - 1][columnIndex];
             rsl[1] = matrix[stringIndex + 1][columnIndex];
+        } else if (stringIndex != 0) {
+            rsl[0] = matrix[stringIndex - 1][columnIndex];
+        } else if (stringIndex + 1 < matrix.length) {
+            rsl[0] = matrix[stringIndex + 1][columnIndex];
         }
         return rsl;
     }
